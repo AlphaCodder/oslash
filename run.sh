ee9412f371cec21e50bf17ebe01b3dacdfa22efb
KUMAR SHIVAM <60447730+AlphaCodder@users.noreply.github.com>
2022-11-03 18:28:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -m main sample_input/input1.txt
-python -m main sample_input/input2.txt
-python -m main sample_input/input3.txt
-python -m main sample_input/input4.txt
+python -m geektrust sample_input/input1.txt
+python -m geektrust sample_input/input2.txt
+python -m geektrust sample_input/input3.txt
+python -m geektrust sample_input/input4.txt
