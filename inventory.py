b7a222f948c617cf94501b37a0c383f6d27792fd
KUMAR SHIVAM <60447730+AlphaCodder@users.noreply.github.com>
2022-11-04 22:20:05 +0530

    Creating separate module for inventory

---
+++ inventory.py
@@ -0,0 +1,10 @@
+# Write a program to calculate the total amount to be paid by the customer.
+products = {"TSHIRT": {"category": "Clothing", "price": 1000, "discount": 10, "max_quantity": 2},
+            "JACKET": {"category": "Clothing", "price": 2000, "discount": 5, "max_quantity": 2},
+            "CAP": {"category": "Clothing", "price": 500, "discount": 20, "max_quantity": 2},
+            "NOTEBOOK": {"category": "Stationery", "price": 200, "discount": 20, "max_quantity": 3},
+            "PENS": {"category": "Stationery", "price": 300, "discount": 10, "max_quantity": 3},
+            "MARKERS": {"category": "Stationery", "price": 500, "discount": 5, "max_quantity": 3}}
+
+total = 0
+discount = 0
